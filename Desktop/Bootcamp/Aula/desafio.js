12187f5182f76334adad4ccb3750477ef468b456
adriano272829 <moliveiramaof@gmail.com>
2020-06-15 23:26:20 -0300

    inicial

---
+++ Desktop/Bootcamp/Aula/desafio.js
@@ -0,0 +1,16 @@
+const nome = 'Adriano'
+const peso = 102
+const altura = 1.85
+
+const imc = peso / (altura * altura)
+
+if (imc >= 30) {
+    console.log(`${nome} você está acima do peso`)
+} else {
+    console.log(`${nome} você não está acima do peso`)
+}
+
+
+
+
+ 
